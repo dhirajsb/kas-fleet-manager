dhirajsb/old-kas-fleet-manager

feat(connectors): replaced cluster with namespace in connector deployments, filtering by namespaces in connector queries

internal/connector/internal/handlers/connector_cluster.go
@@ -32,13 +32,14 @@ var (
 
 type ConnectorClusterHandler struct {
 	di.Inject
-	Bus            signalbus.SignalBus
-	Service        services.ConnectorClusterService
-	Keycloak       coreservices.KafkaKeycloakService
-	ConnectorTypes services.ConnectorTypesService
-	Vault          vault.VaultService
-	KeycloakConfig *keycloak.KeycloakConfig
-	ServerConfig   *server.ServerConfig
+	Bus                signalbus.SignalBus
+	Service            services.ConnectorClusterService
+	Keycloak           coreservices.KafkaKeycloakService
+	ConnectorTypes     services.ConnectorTypesService
+	ConnectorNamespace services.ConnectorNamespaceService
+	Vault              vault.VaultService
+	KeycloakConfig     *keycloak.KeycloakConfig
+	ServerConfig       *server.ServerConfig
 }
 
 func NewConnectorClusterHandler(handler ConnectorClusterHandler) *ConnectorClusterHandler {

dhirajsb/kas-fleet-manager

internal/connector/internal/handlers/connector_cluster.go
@@ -33,13 +33,14 @@ var (
 
 type ConnectorClusterHandler struct {
 	di.Inject
-	Bus            signalbus.SignalBus
-	Service        services.ConnectorClusterService
-	Keycloak       sso.KafkaKeycloakService
-	ConnectorTypes services.ConnectorTypesService
-	Vault          vault.VaultService
-	KeycloakConfig *keycloak.KeycloakConfig
-	ServerConfig   *server.ServerConfig
+	Bus                signalbus.SignalBus
+	Service            services.ConnectorClusterService
+	Keycloak           sso.KafkaKeycloakService
+	ConnectorTypes     services.ConnectorTypesService
+	ConnectorNamespace services.ConnectorNamespaceService
+	Vault              vault.VaultService
+	KeycloakConfig     *keycloak.KeycloakConfig
+	ServerConfig       *server.ServerConfig
 }
 
 func NewConnectorClusterHandler(handler ConnectorClusterHandler) *ConnectorClusterHandler {