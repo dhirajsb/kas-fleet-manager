dhirajsb/old-kas-fleet-manager

feat(connector): added initial apis for connector namespaces

internal/connector/internal/handlers/connector_admin.go
@@ -6,7 +6,6 @@ import (
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/auth"
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/client/keycloak"
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/pkg/server"
-
 	"net/http"
 
 	"github.com/bf2fc6cc711aee1a0c2a/kas-fleet-manager/internal/connector/internal/api/dbapi"
@@ -23,13 +22,14 @@ import (
 
 type ConnectorAdminHandler struct {
 	di.Inject
-	Bus            signalbus.SignalBus
-	Service        services.ConnectorClusterService
-	Keycloak       coreservices.KafkaKeycloakService
-	ConnectorTypes services.ConnectorTypesService
-	Vault          vault.VaultService
-	KeycloakConfig *keycloak.KeycloakConfig
-	ServerConfig   *server.ServerConfig
+	Bus              signalbus.SignalBus
+	Service          services.ConnectorClusterService
+	NamespaceService services.ConnectorNamespaceService
+	Keycloak         coreservices.KafkaKeycloakService
+	ConnectorTypes   services.ConnectorTypesService
+	Vault            vault.VaultService
+	KeycloakConfig   *keycloak.KeycloakConfig
+	ServerConfig     *server.ServerConfig
 }
 
 func NewConnectorAdminHandler(handler ConnectorAdminHandler) *ConnectorAdminHandler {
@@ -57,9 +57,9 @@ func (h *ConnectorAdminHandler) ListConnectorClusters(w http.ResponseWriter, r *
 				Total: int32(paging.Total),
 			}
 
-			for _, resource := range resources {
-				converted := presenters.PresentPrivateConnectorCluster(resource)
-				resourceList.Items = append(resourceList.Items, converted)
+			resourceList.Items = make([]private.ConnectorCluster, len(resources))
+			for i, resource := range resources {
+				resourceList.Items[i] = presenters.PresentPrivateConnectorCluster(resource)
 			}
 
 			return resourceList, nil
@@ -86,8 +86,8 @@ func (h *ConnectorAdminHandler) GetConnectorUpgradesByType(writer http.ResponseW
 				return nil, serviceError
 			}
 			result := make([]private.ConnectorAvailableTypeUpgrade, len(upgrades))
-			for i, upgrade := range upgrades {
-				result[i] = *presenters.PresentConnectorAvailableTypeUpgrade(&upgrade)
+			for j, upgrade := range upgrades {
+				result[j] = *presenters.PresentConnectorAvailableTypeUpgrade(&upgrade)
 			}
 
 			i = private.ConnectorAvailableTypeUpgradeList{
@@ -101,19 +101,6 @@ func (h *ConnectorAdminHandler) GetConnectorUpgradesByType(writer http.ResponseW
 	}
 
 	handlers.HandleGet(writer, request, &cfg)
-}
-
-func isAdmin(request *http.Request) *errors.ServiceError {
-	ctx := request.Context()
-	_, err := auth.GetClaimsFromContext(ctx)
-	if err != nil {
-		return errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authenticated")
-	}
-
-	if !auth.GetIsAdminFromContext(ctx) {
-		return errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authorized")
-	}
-	return nil
 }
 
 func (h *ConnectorAdminHandler) UpgradeConnectorsByType(writer http.ResponseWriter, request *http.Request) {
@@ -195,4 +182,100 @@ func (h *ConnectorAdminHandler) UpgradeConnectorsByOperator(writer http.Response
 	}
 
 	handlers.Handle(writer, request, &cfg, http.StatusNoContent)
+}
+
+func (h *ConnectorAdminHandler) GetConnectorNamespaces(writer http.ResponseWriter, request *http.Request) {
+	listArgs := coreservices.NewListArguments(request.URL.Query())
+	cfg := handlers.HandlerConfig{
+		Action: func() (interface{}, *errors.ServiceError) {
+			if err := isAdmin(request); err != nil {
+				return nil, err
+			}
+
+			namespaces, paging, err := h.NamespaceService.List(request.Context(), []string{}, listArgs)
+			if err != nil {
+				return nil, err
+			}
+
+			result := private.ConnectorNamespaceList{
+				Kind:  "ConnectorNamespaceList",
+				Page:  int32(paging.Page),
+				Size:  int32(paging.Size),
+				Total: int32(paging.Total),
+			}
+
+			result.Items = make([]private.ConnectorNamespace, len(namespaces))
+			for i, namespace := range namespaces {
+				result.Items[i] = presenters.PresentPrivateConnectorNamespace(namespace)
+			}
+
+			return result, nil
+		},
+	}
+
+	handlers.HandleGet(writer, request, &cfg)
+}
+
+func (h *ConnectorAdminHandler) CreateConnectorNamespace(writer http.ResponseWriter, request *http.Request) {
+	var resource private.ConnectorNamespaceWithTenantRequest
+	cfg := handlers.HandlerConfig{
+		MarshalInto: &resource,
+		Action: func() (i interface{}, serviceError *errors.ServiceError) {
+			if serviceError = isAdmin(request); serviceError != nil {
+				return nil, serviceError
+			}
+
+			ctx := request.Context()
+			connectorNamespace := presenters.ConvertConnectorNamespaceWithTenantRequest(&resource)
+			if connectorNamespace.TenantUserId != nil {
+				connectorNamespace.Owner = *connectorNamespace.TenantUserId
+			} else {
+				// NOTE: admin user is owner
+				claims, err := auth.GetClaimsFromContext(ctx)
+				if err != nil {
+					return nil, errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authenticated")
+				}
+				connectorNamespace.Owner = auth.GetUsernameFromClaims(claims)
+			}
+			if err := h.NamespaceService.Create(ctx, connectorNamespace); err != nil {
+				return nil, err
+			}
+			i = presenters.PresentPrivateConnectorNamespace(connectorNamespace)
+			return
+		},
+	}
+
+	handlers.Handle(writer, request, &cfg, http.StatusCreated)
+}
+
+func (h *ConnectorAdminHandler) DeleteConnectorNamespace(writer http.ResponseWriter, request *http.Request) {
+	namespaceId := mux.Vars(request)["namespace_id"]
+	cfg := handlers.HandlerConfig{
+		Validate: []handlers.Validate{
+			handlers.Validation("namespace_id", &namespaceId, handlers.MinLen(1), handlers.MaxLen(maxConnectorNamespaceIdLength)),
+		},
+		Action: func() (i interface{}, serviceError *errors.ServiceError) {
+			if serviceError = isAdmin(request); serviceError != nil {
+				return nil, serviceError
+			}
+
+			serviceError = h.NamespaceService.Delete(request.Context(), namespaceId)
+			return nil, serviceError
+		},
+	}
+
+	handlers.HandleDelete(writer, request, &cfg, http.StatusNoContent)
+}
+
+func isAdmin(request *http.Request) *errors.ServiceError {
+	ctx := request.Context()
+	_, err := auth.GetClaimsFromContext(ctx)
+	if err != nil {
+		return errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authenticated")
+	}
+
+	if !auth.GetIsAdminFromContext(ctx) {
+		return errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authorized")
+	}
+	return nil
 }

internal/connector/internal/routes/route_loader.go
@@ -23,17 +23,18 @@ import (
 
 type options struct {
 	di.Inject
-	ConnectorsConfig        *config.ConnectorsConfig
-	ServerConfig            *server.ServerConfig
-	ErrorsHandler           *coreHandlers.ErrorHandler
-	AuthorizeMiddleware     *acl.AccessControlListMiddleware
-	KeycloakService         services.KafkaKeycloakService
-	AuthAgentService        auth.AuthAgentService
-	ConnectorAdminHandler   *handlers.ConnectorAdminHandler
-	ConnectorTypesHandler   *handlers.ConnectorTypesHandler
-	ConnectorsHandler       *handlers.ConnectorsHandler
-	ConnectorClusterHandler *handlers.ConnectorClusterHandler
-	DB                      *db.ConnectionFactory
+	ConnectorsConfig          *config.ConnectorsConfig
+	ServerConfig              *server.ServerConfig
+	ErrorsHandler             *coreHandlers.ErrorHandler
+	AuthorizeMiddleware       *acl.AccessControlListMiddleware
+	KeycloakService           services.KafkaKeycloakService
+	AuthAgentService          auth.AuthAgentService
+	ConnectorAdminHandler     *handlers.ConnectorAdminHandler
+	ConnectorTypesHandler     *handlers.ConnectorTypesHandler
+	ConnectorsHandler         *handlers.ConnectorsHandler
+	ConnectorClusterHandler   *handlers.ConnectorClusterHandler
+	ConnectorNamespaceHandler *handlers.ConnectorNamespaceHandler
+	DB                        *db.ConnectionFactory
 }
 
 func NewRouteLoader(s options) environments.RouteLoader {
@@ -121,6 +122,21 @@ func (s *options) AddRoutes(mainRouter *mux.Router) error {
 	apiV1ConnectorClustersRouter.HandleFunc("/{connector_cluster_id}/{_:addon[-_]parameters}", s.ConnectorClusterHandler.GetAddonParameters).Methods(http.MethodGet)
 	apiV1ConnectorClustersRouter.Use(s.AuthorizeMiddleware.Authorize)
 
+	//  /api/connector_mgmt/v1/kafka_connector_namespaces
+	v1Collections = append(v1Collections, api.CollectionMetadata{
+		ID:   "kafka_connector_namespaces",
+		Kind: "ConnectorNamespaceList",
+	})
+
+	apiV1ConnectorNamespacesRouter := apiV1Router.PathPrefix("/{_:kafka[-_]connector[-_]namespaces}").Subrouter()
+	apiV1ConnectorNamespacesRouter.HandleFunc("", s.ConnectorNamespaceHandler.Create).Methods(http.MethodPost)
+	apiV1ConnectorNamespacesRouter.HandleFunc("/eval", s.ConnectorNamespaceHandler.CreateEvaluation).Methods(http.MethodPost)
+	apiV1ConnectorNamespacesRouter.HandleFunc("", s.ConnectorNamespaceHandler.List).Methods(http.MethodGet)
+	apiV1ConnectorNamespacesRouter.HandleFunc("/{connector_namespace_id}", s.ConnectorNamespaceHandler.Get).Methods(http.MethodGet)
+	apiV1ConnectorNamespacesRouter.HandleFunc("/{connector_namespace_id}", s.ConnectorNamespaceHandler.Update).Methods(http.MethodPut)
+	apiV1ConnectorNamespacesRouter.HandleFunc("/{connector_namespace_id}", s.ConnectorNamespaceHandler.Delete).Methods(http.MethodDelete)
+	apiV1ConnectorNamespacesRouter.Use(s.AuthorizeMiddleware.Authorize)
+
 	// This section adds the API's accessed by the connector agent...
 	{
 		//  /api/connector_mgmt/v1/kafka_connector_clusters/{id}
@@ -133,19 +149,24 @@ func (s *options) AddRoutes(mainRouter *mux.Router) error {
 	}
 
 	// This section adds APIs accessed by connector admins
-	adminRouter := apiV1Router.PathPrefix("/admin/{_:kafka[-_]connector[-_]clusters}").Subrouter()
+	adminRouter := apiV1Router.PathPrefix("/admin").Subrouter()
 	rolesMapping := map[string][]string{
-		http.MethodGet: {auth.ConnectorFleetManagerAdminReadRole, auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
-		http.MethodPut: {auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
+		http.MethodDelete: {auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
+		http.MethodGet:    {auth.ConnectorFleetManagerAdminReadRole, auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
+		http.MethodPost:   {auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
+		http.MethodPut:    {auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
 	}
 	adminRouter.Use(auth.NewRequireIssuerMiddleware().RequireIssuer([]string{s.KeycloakService.GetConfig().OSDClusterIDPRealm.ValidIssuerURI}, kerrors.ErrorNotFound))
 	adminRouter.Use(auth.NewRolesAuhzMiddleware().RequireRolesForMethods(rolesMapping, kerrors.ErrorNotFound))
 	adminRouter.Use(auth.NewAuditLogMiddleware().AuditLog(kerrors.ErrorNotFound))
-	adminRouter.HandleFunc("", s.ConnectorAdminHandler.ListConnectorClusters).Methods(http.MethodGet)
-	adminRouter.HandleFunc("/{connector_cluster_id}/upgrades/type", s.ConnectorAdminHandler.GetConnectorUpgradesByType).Methods(http.MethodGet)
-	adminRouter.HandleFunc("/{connector_cluster_id}/upgrades/type", s.ConnectorAdminHandler.UpgradeConnectorsByType).Methods(http.MethodPut)
-	adminRouter.HandleFunc("/{connector_cluster_id}/upgrades/operator", s.ConnectorAdminHandler.GetConnectorUpgradesByOperator).Methods(http.MethodGet)
-	adminRouter.HandleFunc("/{connector_cluster_id}/upgrades/operator", s.ConnectorAdminHandler.UpgradeConnectorsByOperator).Methods(http.MethodPut)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}", s.ConnectorAdminHandler.ListConnectorClusters).Methods(http.MethodGet)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}/{connector_cluster_id}/upgrades/type", s.ConnectorAdminHandler.GetConnectorUpgradesByType).Methods(http.MethodGet)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}/{connector_cluster_id}/upgrades/type", s.ConnectorAdminHandler.UpgradeConnectorsByType).Methods(http.MethodPut)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}/{connector_cluster_id}/upgrades/operator", s.ConnectorAdminHandler.GetConnectorUpgradesByOperator).Methods(http.MethodGet)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}/{connector_cluster_id}/upgrades/operator", s.ConnectorAdminHandler.UpgradeConnectorsByOperator).Methods(http.MethodPut)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]namespaces}", s.ConnectorAdminHandler.GetConnectorNamespaces).Methods(http.MethodGet)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]namespaces}", s.ConnectorAdminHandler.CreateConnectorNamespace).Methods(http.MethodPost)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]namespaces}/{namespace_id}", s.ConnectorAdminHandler.DeleteConnectorNamespace).Methods(http.MethodDelete)
 
 	v1Metadata := api.VersionMetadata{
 		ID:          "v1",

dhirajsb/kas-fleet-manager

internal/connector/internal/handlers/connector_admin.go
@@ -24,13 +24,14 @@ import (
 
 type ConnectorAdminHandler struct {
 	di.Inject
-	Bus            signalbus.SignalBus
-	Service        services.ConnectorClusterService
-	Keycloak       sso.KafkaKeycloakService
-	ConnectorTypes services.ConnectorTypesService
-	Vault          vault.VaultService
-	KeycloakConfig *keycloak.KeycloakConfig
-	ServerConfig   *server.ServerConfig
+	Bus              signalbus.SignalBus
+	Service          services.ConnectorClusterService
+	NamespaceService services.ConnectorNamespaceService
+	Keycloak         sso.KafkaKeycloakService
+	ConnectorTypes   services.ConnectorTypesService
+	Vault            vault.VaultService
+	KeycloakConfig   *keycloak.KeycloakConfig
+	ServerConfig     *server.ServerConfig
 }
 
 func NewConnectorAdminHandler(handler ConnectorAdminHandler) *ConnectorAdminHandler {
@@ -58,9 +59,9 @@ func (h *ConnectorAdminHandler) ListConnectorClusters(w http.ResponseWriter, r *
 				Total: int32(paging.Total),
 			}
 
-			for _, resource := range resources {
-				converted := presenters.PresentPrivateConnectorCluster(resource)
-				resourceList.Items = append(resourceList.Items, converted)
+			resourceList.Items = make([]private.ConnectorCluster, len(resources))
+			for i, resource := range resources {
+				resourceList.Items[i] = presenters.PresentPrivateConnectorCluster(resource)
 			}
 
 			return resourceList, nil
@@ -87,8 +88,8 @@ func (h *ConnectorAdminHandler) GetConnectorUpgradesByType(writer http.ResponseW
 				return nil, serviceError
 			}
 			result := make([]private.ConnectorAvailableTypeUpgrade, len(upgrades))
-			for i, upgrade := range upgrades {
-				result[i] = *presenters.PresentConnectorAvailableTypeUpgrade(&upgrade)
+			for j, upgrade := range upgrades {
+				result[j] = *presenters.PresentConnectorAvailableTypeUpgrade(&upgrade)
 			}
 
 			i = private.ConnectorAvailableTypeUpgradeList{
@@ -102,19 +103,6 @@ func (h *ConnectorAdminHandler) GetConnectorUpgradesByType(writer http.ResponseW
 	}
 
 	handlers.HandleGet(writer, request, &cfg)
-}
-
-func isAdmin(request *http.Request) *errors.ServiceError {
-	ctx := request.Context()
-	_, err := auth.GetClaimsFromContext(ctx)
-	if err != nil {
-		return errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authenticated")
-	}
-
-	if !auth.GetIsAdminFromContext(ctx) {
-		return errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authorized")
-	}
-	return nil
 }
 
 func (h *ConnectorAdminHandler) UpgradeConnectorsByType(writer http.ResponseWriter, request *http.Request) {
@@ -196,4 +184,100 @@ func (h *ConnectorAdminHandler) UpgradeConnectorsByOperator(writer http.Response
 	}
 
 	handlers.Handle(writer, request, &cfg, http.StatusNoContent)
+}
+
+func (h *ConnectorAdminHandler) GetConnectorNamespaces(writer http.ResponseWriter, request *http.Request) {
+	listArgs := coreservices.NewListArguments(request.URL.Query())
+	cfg := handlers.HandlerConfig{
+		Action: func() (interface{}, *errors.ServiceError) {
+			if err := isAdmin(request); err != nil {
+				return nil, err
+			}
+
+			namespaces, paging, err := h.NamespaceService.List(request.Context(), []string{}, listArgs)
+			if err != nil {
+				return nil, err
+			}
+
+			result := private.ConnectorNamespaceList{
+				Kind:  "ConnectorNamespaceList",
+				Page:  int32(paging.Page),
+				Size:  int32(paging.Size),
+				Total: int32(paging.Total),
+			}
+
+			result.Items = make([]private.ConnectorNamespace, len(namespaces))
+			for i, namespace := range namespaces {
+				result.Items[i] = presenters.PresentPrivateConnectorNamespace(namespace)
+			}
+
+			return result, nil
+		},
+	}
+
+	handlers.HandleGet(writer, request, &cfg)
+}
+
+func (h *ConnectorAdminHandler) CreateConnectorNamespace(writer http.ResponseWriter, request *http.Request) {
+	var resource private.ConnectorNamespaceWithTenantRequest
+	cfg := handlers.HandlerConfig{
+		MarshalInto: &resource,
+		Action: func() (i interface{}, serviceError *errors.ServiceError) {
+			if serviceError = isAdmin(request); serviceError != nil {
+				return nil, serviceError
+			}
+
+			ctx := request.Context()
+			connectorNamespace := presenters.ConvertConnectorNamespaceWithTenantRequest(&resource)
+			if connectorNamespace.TenantUserId != nil {
+				connectorNamespace.Owner = *connectorNamespace.TenantUserId
+			} else {
+				// NOTE: admin user is owner
+				claims, err := auth.GetClaimsFromContext(ctx)
+				if err != nil {
+					return nil, errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authenticated")
+				}
+				connectorNamespace.Owner = auth.GetUsernameFromClaims(claims)
+			}
+			if err := h.NamespaceService.Create(ctx, connectorNamespace); err != nil {
+				return nil, err
+			}
+			i = presenters.PresentPrivateConnectorNamespace(connectorNamespace)
+			return
+		},
+	}
+
+	handlers.Handle(writer, request, &cfg, http.StatusCreated)
+}
+
+func (h *ConnectorAdminHandler) DeleteConnectorNamespace(writer http.ResponseWriter, request *http.Request) {
+	namespaceId := mux.Vars(request)["namespace_id"]
+	cfg := handlers.HandlerConfig{
+		Validate: []handlers.Validate{
+			handlers.Validation("namespace_id", &namespaceId, handlers.MinLen(1), handlers.MaxLen(maxConnectorNamespaceIdLength)),
+		},
+		Action: func() (i interface{}, serviceError *errors.ServiceError) {
+			if serviceError = isAdmin(request); serviceError != nil {
+				return nil, serviceError
+			}
+
+			serviceError = h.NamespaceService.Delete(request.Context(), namespaceId)
+			return nil, serviceError
+		},
+	}
+
+	handlers.HandleDelete(writer, request, &cfg, http.StatusNoContent)
+}
+
+func isAdmin(request *http.Request) *errors.ServiceError {
+	ctx := request.Context()
+	_, err := auth.GetClaimsFromContext(ctx)
+	if err != nil {
+		return errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authenticated")
+	}
+
+	if !auth.GetIsAdminFromContext(ctx) {
+		return errors.NewWithCause(errors.ErrorUnauthenticated, err, "user not authorized")
+	}
+	return nil
 }

internal/connector/internal/routes/route_loader.go
@@ -24,17 +24,18 @@ import (
 
 type options struct {
 	di.Inject
-	ConnectorsConfig        *config.ConnectorsConfig
-	ServerConfig            *server.ServerConfig
-	ErrorsHandler           *coreHandlers.ErrorHandler
-	AuthorizeMiddleware     *acl.AccessControlListMiddleware
-	KeycloakService         sso.KafkaKeycloakService
-	AuthAgentService        auth.AuthAgentService
-	ConnectorAdminHandler   *handlers.ConnectorAdminHandler
-	ConnectorTypesHandler   *handlers.ConnectorTypesHandler
-	ConnectorsHandler       *handlers.ConnectorsHandler
-	ConnectorClusterHandler *handlers.ConnectorClusterHandler
-	DB                      *db.ConnectionFactory
+	ConnectorsConfig          *config.ConnectorsConfig
+	ServerConfig              *server.ServerConfig
+	ErrorsHandler             *coreHandlers.ErrorHandler
+	AuthorizeMiddleware       *acl.AccessControlListMiddleware
+	KeycloakService           sso.KafkaKeycloakService
+	AuthAgentService          auth.AuthAgentService
+	ConnectorAdminHandler     *handlers.ConnectorAdminHandler
+	ConnectorTypesHandler     *handlers.ConnectorTypesHandler
+	ConnectorsHandler         *handlers.ConnectorsHandler
+	ConnectorClusterHandler   *handlers.ConnectorClusterHandler
+	ConnectorNamespaceHandler *handlers.ConnectorNamespaceHandler
+	DB                        *db.ConnectionFactory
 }
 
 func NewRouteLoader(s options) environments.RouteLoader {
@@ -111,6 +112,21 @@ func (s *options) AddRoutes(mainRouter *mux.Router) error {
 	apiV1ConnectorClustersRouter.HandleFunc("/{connector_cluster_id}/{_:addon[-_]parameters}", s.ConnectorClusterHandler.GetAddonParameters).Methods(http.MethodGet)
 	apiV1ConnectorClustersRouter.Use(s.AuthorizeMiddleware.Authorize)
 
+	//  /api/connector_mgmt/v1/kafka_connector_namespaces
+	v1Collections = append(v1Collections, api.CollectionMetadata{
+		ID:   "kafka_connector_namespaces",
+		Kind: "ConnectorNamespaceList",
+	})
+
+	apiV1ConnectorNamespacesRouter := apiV1Router.PathPrefix("/{_:kafka[-_]connector[-_]namespaces}").Subrouter()
+	apiV1ConnectorNamespacesRouter.HandleFunc("", s.ConnectorNamespaceHandler.Create).Methods(http.MethodPost)
+	apiV1ConnectorNamespacesRouter.HandleFunc("/eval", s.ConnectorNamespaceHandler.CreateEvaluation).Methods(http.MethodPost)
+	apiV1ConnectorNamespacesRouter.HandleFunc("", s.ConnectorNamespaceHandler.List).Methods(http.MethodGet)
+	apiV1ConnectorNamespacesRouter.HandleFunc("/{connector_namespace_id}", s.ConnectorNamespaceHandler.Get).Methods(http.MethodGet)
+	apiV1ConnectorNamespacesRouter.HandleFunc("/{connector_namespace_id}", s.ConnectorNamespaceHandler.Update).Methods(http.MethodPut)
+	apiV1ConnectorNamespacesRouter.HandleFunc("/{connector_namespace_id}", s.ConnectorNamespaceHandler.Delete).Methods(http.MethodDelete)
+	apiV1ConnectorNamespacesRouter.Use(s.AuthorizeMiddleware.Authorize)
+
 	// This section adds the API's accessed by the connector agent...
 	{
 		//  /api/connector_mgmt/v1/kafka_connector_clusters/{id}
@@ -123,19 +139,24 @@ func (s *options) AddRoutes(mainRouter *mux.Router) error {
 	}
 
 	// This section adds APIs accessed by connector admins
-	adminRouter := apiV1Router.PathPrefix("/admin/{_:kafka[-_]connector[-_]clusters}").Subrouter()
+	adminRouter := apiV1Router.PathPrefix("/admin").Subrouter()
 	rolesMapping := map[string][]string{
-		http.MethodGet: {auth.ConnectorFleetManagerAdminReadRole, auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
-		http.MethodPut: {auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
+		http.MethodDelete: {auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
+		http.MethodGet:    {auth.ConnectorFleetManagerAdminReadRole, auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
+		http.MethodPost:   {auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
+		http.MethodPut:    {auth.ConnectorFleetManagerAdminWriteRole, auth.ConnectorFleetManagerAdminFullRole},
 	}
 	adminRouter.Use(auth.NewRequireIssuerMiddleware().RequireIssuer([]string{s.KeycloakService.GetConfig().OSDClusterIDPRealm.ValidIssuerURI}, kerrors.ErrorNotFound))
 	adminRouter.Use(auth.NewRolesAuhzMiddleware().RequireRolesForMethods(rolesMapping, kerrors.ErrorNotFound))
 	adminRouter.Use(auth.NewAuditLogMiddleware().AuditLog(kerrors.ErrorNotFound))
-	adminRouter.HandleFunc("", s.ConnectorAdminHandler.ListConnectorClusters).Methods(http.MethodGet)
-	adminRouter.HandleFunc("/{connector_cluster_id}/upgrades/type", s.ConnectorAdminHandler.GetConnectorUpgradesByType).Methods(http.MethodGet)
-	adminRouter.HandleFunc("/{connector_cluster_id}/upgrades/type", s.ConnectorAdminHandler.UpgradeConnectorsByType).Methods(http.MethodPut)
-	adminRouter.HandleFunc("/{connector_cluster_id}/upgrades/operator", s.ConnectorAdminHandler.GetConnectorUpgradesByOperator).Methods(http.MethodGet)
-	adminRouter.HandleFunc("/{connector_cluster_id}/upgrades/operator", s.ConnectorAdminHandler.UpgradeConnectorsByOperator).Methods(http.MethodPut)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}", s.ConnectorAdminHandler.ListConnectorClusters).Methods(http.MethodGet)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}/{connector_cluster_id}/upgrades/type", s.ConnectorAdminHandler.GetConnectorUpgradesByType).Methods(http.MethodGet)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}/{connector_cluster_id}/upgrades/type", s.ConnectorAdminHandler.UpgradeConnectorsByType).Methods(http.MethodPut)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}/{connector_cluster_id}/upgrades/operator", s.ConnectorAdminHandler.GetConnectorUpgradesByOperator).Methods(http.MethodGet)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]clusters}/{connector_cluster_id}/upgrades/operator", s.ConnectorAdminHandler.UpgradeConnectorsByOperator).Methods(http.MethodPut)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]namespaces}", s.ConnectorAdminHandler.GetConnectorNamespaces).Methods(http.MethodGet)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]namespaces}", s.ConnectorAdminHandler.CreateConnectorNamespace).Methods(http.MethodPost)
+	adminRouter.HandleFunc("/{_:kafka[-_]connector[-_]namespaces}/{namespace_id}", s.ConnectorAdminHandler.DeleteConnectorNamespace).Methods(http.MethodDelete)
 
 	v1Metadata := api.VersionMetadata{
 		ID:          "v1",